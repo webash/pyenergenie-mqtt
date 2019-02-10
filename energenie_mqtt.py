68751ae5cce3317ff0c78bcc97cc478277deeb3e
Ashley <webash@users.noreply.github.com>
2019-02-10 20:37:58 +0000

    I don't understand scope in Python...
    And I think that's what's causing my devices to always have no apparent_power() despite just receiving a message from them...

--- energenie_mqtt.py
+++ energenie_mqtt.py
@@ -26,173 +26,11 @@ PRODUCTID_MIHO004                = 0x01   #         Monitor only
 PRODUCTID_MIHO005                = 0x02   #         Adaptor Plus
 PRODUCTID_MIHO006                = 0x05   #         House Monitor
 
-
 q_rx_mqtt = Queue.Queue()
 q_rx_energenie = Queue.Queue()
 q_tx_mqtt = Queue.Queue()
 #q_tx_energenie = Queue.Queue() # Not yet in use, TODO
 
-def rx_mqtt():
-	global mqtt_hostname
-	global mqtt_port
-	global mqtt_keepalive
-	global mqtt_username
-	global mqtt_password
-	global mqtt_client_id
-	global mqtt_clean_session
-	global mqtt_subscribe_topic
-	global q_rx_mqtt
-
-	# The callback for when the client receives a CONNACK response from the server.
-	def on_connect(client, userdata, flags, rc):
-		global mqtt_subscribe_topic
-		print("Connected with result code "+str(rc))
-
-		# Subscribing in on_connect() means that if we lose the connection and
-		# reconnect then subscriptions will be renewed.
-		print("Subscribing to "+mqtt_subscribe_topic+"/#")
-		client.subscribe(mqtt_subscribe_topic + "/#")
-
-	# The callback for when a PUBLISH message is received from the server.
-	def on_message(client, userdata, msg):
-		global q_rx_mqtt
-		q_rx_mqtt.put(msg)
-
-
-
-	print("Starting mqtt subscribing loop...")
-	while True:
-		try:
-			client = mqtt.Client(client_id=mqtt_client_id, clean_session=mqtt_clean_session)
-			client.on_connect = on_connect
-			client.on_message = on_message
-
-			if mqtt_username != "":
-				client.username_pw_set(mqtt_username, mqtt_password)
-			client.connect(mqtt_hostname, mqtt_port, mqtt_keepalive)
-
-			# Blocking call that processes network traffic, dispatches callbacks and
-			# handles reconnecting.
-			# Other loop*() functions are available that give a threaded interface and a
-			# manual interface.
-			client.loop_forever()
-		finally:
-			print("Restarting...")
-
-
-def mqtt_tx_energenie():
-	global q_rx_mqtt
-
-	while True:
-		try:
-			msg = q_rx_mqtt.get()
-			print(msg.topic+" "+str(msg.payload))
-			name = msg.topic.split("/", 2)[1]
-			device = energenie.registry.get(name)
-			if str(msg.payload) == "1":
-				print(name+" on")
-				for x in range(0, 5):
-					device.turn_on()
-					time.sleep(0.1)
-			else:
-				print(name+" off")
-				for x in range(0, 5):
-					device.turn_off()
-					time.sleep(0.1)
-		except:
-			print("Got exception")
-		finally:
-			q_rx_mqtt.task_done()
-			
-def rx_energenie(address, message):
-	global q_rx_energenie
-
-	print("rx_energenie: new message from " + str(address) )
-
-	if address[0] == MFRID_ENERGENIE:
-		# Retrieve list of names from the registry, so we can refer to the name of the device
-		for devicename in energenie.registry.names():
-			print("rx_energenie: checking if message from " + devicename)
-
-			# Using the name, retrieve the device
-			d = energenie.registry.get(devicename)
-
-			# Check if the message is from the current device of this iteration
-			if address[2] == d.get_device_id():
-				# Yes we found the device, so add to processing queue
-				print("rx_energenie: YES; add to process queue")
-				newQueueEntry = {'DeviceName': devicename, 'DeviceType': address[1]}
-				q_rx_energenie.put(newQueueEntry)
-				# The device was found, so break from the for loop
-				break
-	else:
-		print("Not an energenie device...?")
-
-
-def rx_energenie_process():
-	global q_rx_energenie
-
-	while True:
-		print("rx_energenie_process: awaiting item in q_rx_energenie...")
-		refreshed_device = q_rx_energenie.get()
-		d = energenie.registry.get( refreshed_device['DeviceName'] )
-
-		print("rx_energenie_process: " + refreshed_device['DeviceName'] + " (type: " + str(refreshed_device['DeviceType']) + ") process beginning...")
-		if refreshed_device['DeviceType'] == PRODUCTID_MIHO006:
-			try:
-				p = d.get_apparent_power()
-				print("Power MIHO006: %s" % str(p))
-				item = {'DeviceName': refreshed_device['DeviceName'], 'data': {"apparent_power": str(p)}}
-				q_tx_mqtt.put(item)
-			except Exception as e:
-				print("rx_energenie_process: Exception getting power")
-				print(e)
-		elif refreshed_device['DeviceType'] == PRODUCTID_MIHO005:
-			try:
-				p = d.get_apparent_power()
-				print("Power MIHO005: %s" % str(p))
-				item = {'DeviceName': refreshed_device['DeviceName'], 'data': {"apparent_power": str(p)}}
-				q_tx_mqtt.put(item)
-			except Exception as e:
-				print("rx_energenie_process: Exception getting power ")
-				print(e)
-		else:
-			print("rx_energenie_process: NOPE; No process defined for " + refreshed_device['DeviceName'] + " of type " + str(refreshed_device['DeviceType']))
-		q_rx_energenie.task_done()
-
-
-
-			
-def energenie_tx_mqtt():
-	global mqtt_hostname
-	global mqtt_port
-	global mqtt_keepalive
-	global mqtt_username
-	global mqtt_password
-	global mqtt_client_id
-	global mqtt_clean_session
-	global mqtt_publish_topic
-	global q_tx_mqtt
-
-	print("energenie_tx_mqtt: creating mqtt.client...")
-	toMqtt = mqtt.Client(client_id=mqtt_client_id, clean_session=mqtt_clean_session)
-
-	if mqtt_username <> "":
-		print("energenie_tx_mqtt: using username and password...")
-		toMqtt.username_pw_set(mqtt_username, mqtt_password)
-	print("energenie_tx_mqtt: connecting to mqtt broker...")
-	toMqtt.connect(mqtt_hostname, mqtt_port, mqtt_keepalive)
-	
-	while True:
-		print("energenie_tx_mqtt: awaiting item in q_tx_mqtt...")
-		item = q_tx_mqtt.get()
-		print("energenie_tx_mqtt: item for" + item['DeviceName'] + " found on queue...")
-		print(str(item))
-		for metric, value in data:
-			publish_topic = mqtt_publish_topic + "/" + item['DeviceName'] + "/" + metric
-			print("energenie_tx_mqtt: publishing " + str(value) + " to topic " + publish_topic)
-			toMqtt.publish(publish_topic, value)
-		q_tx_mqtt.task_done()
 
 def main():
 	global mqtt_hostname
@@ -202,6 +40,169 @@ def main():
 	global mqtt_password
 	global mqtt_client_id
 	global mqtt_clean_session
+	global mqtt_publish_topic
+	global mqtt_subscribe_topic
+
+	def rx_mqtt():
+		global mqtt_hostname
+		global mqtt_port
+		global mqtt_keepalive
+		global mqtt_username
+		global mqtt_password
+		global mqtt_client_id
+		global mqtt_clean_session
+		global mqtt_subscribe_topic
+		global q_rx_mqtt
+
+		# The callback for when the client receives a CONNACK response from the server.
+		def on_connect(client, userdata, flags, rc):
+			global mqtt_subscribe_topic
+			print("Connected with result code "+str(rc))
+
+			# Subscribing in on_connect() means that if we lose the connection and
+			# reconnect then subscriptions will be renewed.
+			print("Subscribing to "+mqtt_subscribe_topic+"/#")
+			client.subscribe(mqtt_subscribe_topic + "/#")
+
+		# The callback for when a PUBLISH message is received from the server.
+		def on_message(client, userdata, msg):
+			global q_rx_mqtt
+			q_rx_mqtt.put(msg)
+
+
+
+		print("Starting mqtt subscribing loop...")
+		while True:
+			try:
+				client = mqtt.Client(client_id=mqtt_client_id, clean_session=mqtt_clean_session)
+				client.on_connect = on_connect
+				client.on_message = on_message
+
+				if mqtt_username != "":
+					client.username_pw_set(mqtt_username, mqtt_password)
+				client.connect(mqtt_hostname, mqtt_port, mqtt_keepalive)
+
+				# Blocking call that processes network traffic, dispatches callbacks and
+				# handles reconnecting.
+				# Other loop*() functions are available that give a threaded interface and a
+				# manual interface.
+				client.loop_forever()
+			finally:
+				print("Restarting...")
+
+
+	def mqtt_tx_energenie():
+		global q_rx_mqtt
+
+		while True:
+			try:
+				msg = q_rx_mqtt.get()
+				print(msg.topic+" "+str(msg.payload))
+				name = msg.topic.split("/", 2)[1]
+				device = energenie.registry.get(name)
+				if str(msg.payload) == "1":
+					print(name+" on")
+					for x in range(0, 5):
+						device.turn_on()
+						time.sleep(0.1)
+				else:
+					print(name+" off")
+					for x in range(0, 5):
+						device.turn_off()
+						time.sleep(0.1)
+			except:
+				print("Got exception")
+			finally:
+				q_rx_mqtt.task_done()
+				
+	def rx_energenie(address, message):
+		global q_rx_energenie
+
+		print("rx_energenie: new message from " + str(address) )
+
+		if address[0] == MFRID_ENERGENIE:
+			# Retrieve list of names from the registry, so we can refer to the name of the device
+			for devicename in energenie.registry.names():
+				print("rx_energenie: checking if message from " + devicename)
+
+				# Using the name, retrieve the device
+				d = energenie.registry.get(devicename)
+
+				# Check if the message is from the current device of this iteration
+				if address[2] == d.get_device_id():
+					# Yes we found the device, so add to processing queue
+					print("rx_energenie: YES; add to process queue")
+					newQueueEntry = {'DeviceName': devicename, 'DeviceType': address[1]}
+					q_rx_energenie.put(newQueueEntry)
+					# The device was found, so break from the for loop
+					break
+		else:
+			print("Not an energenie device...?")
+
+
+	def rx_energenie_process():
+		global q_rx_energenie
+
+		while True:
+			print("rx_energenie_process: awaiting item in q_rx_energenie...")
+			refreshed_device = q_rx_energenie.get()
+			d = energenie.registry.get( refreshed_device['DeviceName'] )
+
+			print("rx_energenie_process: " + refreshed_device['DeviceName'] + " (type: " + str(refreshed_device['DeviceType']) + ") process beginning...")
+			if refreshed_device['DeviceType'] == PRODUCTID_MIHO006:
+				try:
+					p = d.get_apparent_power()
+					print("Power MIHO006: %s" % str(p))
+					item = {'DeviceName': refreshed_device['DeviceName'], 'data': {"apparent_power": str(p)}}
+					q_tx_mqtt.put(item)
+				except Exception as e:
+					print("rx_energenie_process: Exception getting power")
+					print(e)
+			elif refreshed_device['DeviceType'] == PRODUCTID_MIHO005:
+				try:
+					p = d.get_apparent_power()
+					print("Power MIHO005: %s" % str(p))
+					item = {'DeviceName': refreshed_device['DeviceName'], 'data': {"apparent_power": str(p)}}
+					q_tx_mqtt.put(item)
+				except Exception as e:
+					print("rx_energenie_process: Exception getting power ")
+					print(e)
+			else:
+				print("rx_energenie_process: NOPE; No process defined for " + refreshed_device['DeviceName'] + " of type " + str(refreshed_device['DeviceType']))
+			q_rx_energenie.task_done()
+				
+	def energenie_tx_mqtt():
+		global mqtt_hostname
+		global mqtt_port
+		global mqtt_keepalive
+		global mqtt_username
+		global mqtt_password
+		global mqtt_client_id
+		global mqtt_clean_session
+		global mqtt_publish_topic
+		global q_tx_mqtt
+
+		print("energenie_tx_mqtt: creating mqtt.client...")
+		toMqtt = mqtt.Client(client_id=mqtt_client_id, clean_session=mqtt_clean_session)
+
+		if mqtt_username <> "":
+			print("energenie_tx_mqtt: using username and password...")
+			toMqtt.username_pw_set(mqtt_username, mqtt_password)
+		print("energenie_tx_mqtt: connecting to mqtt broker...")
+		toMqtt.connect(mqtt_hostname, mqtt_port, mqtt_keepalive)
+		
+		while True:
+			print("energenie_tx_mqtt: awaiting item in q_tx_mqtt...")
+			item = q_tx_mqtt.get()
+			print("energenie_tx_mqtt: item for" + item['DeviceName'] + " found on queue...")
+			print(str(item))
+			for metric, value in data:
+				publish_topic = mqtt_publish_topic + "/" + item['DeviceName'] + "/" + metric
+				print("energenie_tx_mqtt: publishing " + str(value) + " to topic " + publish_topic)
+				toMqtt.publish(publish_topic, value)
+			q_tx_mqtt.task_done()
+
+			
 	
 	# Start thread for receiving inbound energenie messages
 	#print("Starting rxFromEnergenie thread...")
